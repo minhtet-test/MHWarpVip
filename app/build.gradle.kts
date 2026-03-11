58aa19f8dc8bec4c27a16b603901f68276998d55
MH Min Htet <123883385+minhtet-test@users.noreply.github.com>
2026-03-12 01:40:56 +0630

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,35 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mhwarp.vip"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.mhwarp.vip"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
 dependencies {
-    // သင့် Go AAR ဖိုင်
-    implementation(files("libs/wgwrapper.aar"))
-    
-    // Default dependencies 
+    implementation(files("libs/wgwrapper.aar")) // Go AAR ချိတ်ခြင်း
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    
-    // API မှ ဒေတာယူရန် Coroutines သုံးရန်
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
